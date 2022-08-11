a55dea7f8f3901b2e1692a3d6bcb987072c928ef
Md. Nazmus Salahin Rocky <rocky.bgta@gmail.com>
2022-08-11 20:52:25 +0600

    init

---
+++ src/test/java/com/example/spring/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.spring.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+
+    public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+
+    @Test
+    void contextLoads() {
+        logger.info("Test case executing..");
+        assertEquals(true,true);
+
+    }
+
+}
